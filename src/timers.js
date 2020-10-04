8bd024d0f484acfff65a4ac9c5381c33ba4d7d2b
Kumar Gaurav <kmrgaurav33@gmail.com>
2020-10-04 23:55:30 +0530

    initial commit

---
+++ src/timers.js
@@ -0,0 +1,22 @@
+const waitList = {}
+
+function clearDebounce (id) {
+  if (waitList.id) {
+    clearTimeout(waitList.id)
+    delete waitList.id
+  }
+}
+
+function debounce ({ id, callback, delay = 400}) {
+  if (waitList[id]) {
+    clearTimeout(waitList[id])
+  }
+  waitList[id] = setTimeout(() => {
+    callback()
+  }, delay)
+}
+
+export default {
+  debounce,
+  clearDebounce
+}
