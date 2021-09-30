2262ff578e896fd817c48574ec5330b8ec383617
Kumar Gaurav <kmrgaurav33@gmail.com>
2021-10-01 02:03:45 +0530

    [feat] eslint config added

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'airbnb-base'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    semi: [2, 'never']
+  }
+}
